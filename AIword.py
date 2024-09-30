fdfe87717f887d089f7a998f11dca69f21e664cf
karina-kovalska <89306487+karina-kovalska@users.noreply.github.com>
2024-09-30 10:46:27 +0200

    Update AIword.py

--- AIword.py
+++ AIword.py
@@ -1,9 +1,8 @@
 from groq import Groq
 client = Groq(api_key="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")
+prompt_question_with_context = "Ask a multiple choice question about history, science or pop culture"
 
 for q in range(5):
-    prompt_question_with_context = "Ask a multiple choice question about history, science or pop culture"
-
     chat_completion = client.chat.completions.create(
         messages=[
             {
@@ -19,13 +18,13 @@ for q in range(5):
 
     ans = input()
 
-    prompt_question_with_context = f"Question: {prev_question}\n My answer: {ans}\n If the answer is correct, output Correct, else output Incorrect and nothing else"
+    prompt_question = f"Question: {prev_question}\n My answer: {ans}\n If the answer is correct, output Correct, else output Incorrect and nothing else"
 
     chat_completion = client.chat.completions.create(
         messages=[
             {
                 "role": "user",
-                "content": prompt_question_with_context,
+                "content": prompt_question,
             }
         ],
         model="llama3-8b-8192",
@@ -39,4 +38,3 @@ for q in range(5):
         exit()
 
 print("You won!!!!!!!!")
-
